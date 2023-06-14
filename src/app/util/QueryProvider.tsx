7ba60b3f5bd959b5614a313aa674d8f4ed441322
Ken Taylor <kentaylorappdev@gmail.com>
2023-06-14 06:15:40 -0400

    reinstalled react query

--- src/app/util/QueryProvider.tsx
+++ src/app/util/QueryProvider.tsx
@@ -1,9 +1,9 @@
-// "use client";
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const QueryProvider = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={new QueryClient()}>
+    {children}
+  </QueryClientProvider>
+);
 
-// const QueryProvider = ({ children }: any) => (
-//   <QueryClientProvider client={new QueryClient()}>
-//     {children}
-//   </QueryClientProvider>
-// );
-
-// export default QueryProvider;
+export default QueryProvider;
