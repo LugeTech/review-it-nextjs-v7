49d290f0040ddaf44cdcf75e0d09ac5120296dce
Ken Taylor <kentaylorappdev@gmail.com>
2023-06-14 09:34:20 -0400

    react query in its basic form running - hacky

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { ChangeEvent, use, useEffect, useState } from "react";
 import fakeData from "@/app/fakedata/data.json";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { iReview } from "../util/Interfaces";
+
 interface Suggestion {
   id: number;
   text: string;
@@ -10,30 +13,46 @@ const SearchSuggestions = () => {
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState<Suggestion[]>(fakeData);
 
-  const fetchSuggestions = async (query: string) => {
-    // const response = await fetch(
-    //   `https://api.example.com/suggestions?query=${query}`
-    // );
-    //filter the fakeData array based on the query string
-    const filteredData = fakeData.filter((item) =>
-      item.text.toLowerCase().includes(query.toLowerCase())
-    );
-    // const data = await fakeData;
-    setSuggestions(filteredData.slice(0, 10));
-  };
+  // const getSuggestions = async (query: string) => {
+  //   const data = await fetch(
+  //     `https://api.example.com/suggestions?query=${query}`
+  //   );
+  //   return data.json();
+  //filter the fakeData array based on the query string
+  // const filteredData = fakeData.filter((item) =>
+  //   item.text.toLowerCase().includes(query.toLowerCase())
+  // );
+  // // const data = await fakeData;
+  // setSuggestions(filteredData.slice(0, 10));
+  // };
+
+  const { isLoading, error, data, isFetching } = useQuery({
+    queryKey: ["getSuggestions"],
+    queryFn: async () => {
+      const data = await fetch(
+        `https://api.github.com/repos/tannerlinsley/react-query`
+      );
+      // console.log(await data.json());
+      return await data.json();
+    },
+  });
+
+  if (isLoading) return "Loading...";
+  if (error) return "An error has occurred: " + error;
+  if (data) {
+    console.log(data as []);
+  }
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const newQuery = e.target.value;
     setQuery(newQuery);
-
-    // Fetch suggestions when the query changes
-    fetchSuggestions(newQuery);
   };
 
   const handleSuggestionClick = (suggestion: Suggestion) => {
     // Perform search based on the selected suggestion
     console.log("Performing search with suggestion:", suggestion);
   };
+  const data1 = [data];
 
   return (
     <div>
@@ -43,17 +62,18 @@ const SearchSuggestions = () => {
         value={query}
         onChange={handleInputChange}
       />
+      <div>{isFetching ? "Getting suggestions..." : ""}</div>
       <ul>
         {query && (
           <div className="absolute mt-1 rounded shadow-lg md:w-3/4 z-10">
-            {suggestions.map((suggestion) => (
+            {data1.map((suggestion: any) => (
               <div
                 key={suggestion.id}
                 className="px-4 py-2 cursor-pointer hover:bg-gray-100 last:border-b-0"
                 onClick={() => handleSuggestionClick(suggestion)}
               >
                 <p className="text-gray-500 text-base font-normal">
-                  {suggestion.text}
+                  {suggestion.full_name}
                 </p>
                 <div className=" w-full">
                   <p className="text-gray-400 text-sm font-extralight">
