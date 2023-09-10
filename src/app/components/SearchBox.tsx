475875b1aaa953e0ba5302c773bacb9f21b532c3
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-09-10 08:23:36 -0400

    cleanup and refactor

--- src/app/components/SearchBox.tsx
+++ src/app/components/SearchBox.tsx
@@ -1,34 +1,44 @@
 import React, { useState, useEffect, FC, ChangeEvent } from 'react';
+import { useAtom } from "jotai";
+import { allProductsStore } from "@/app/store/store";
+import { iProduct } from '../util/Interfaces';
 
-interface SearchBoxProps {
-  onSearch: (query: string) => void;
-}
-
-const SearchBox: FC<SearchBoxProps> = ({ onSearch }) => {
+const SearchBox = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(
     null
   );
+  const [allProducts, setAllProducts] = useAtom(allProductsStore);
+
+  const filteredProducts = (products: iProduct[]) => {
+    const fp = products.filter((product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    console.log('input change', fp)
+    return fp;
+  };
 
   useEffect(() => {
     if (typingTimeout) {
       clearTimeout(typingTimeout);
     }
-
     const timeout = setTimeout(() => {
-      onSearch(searchTerm);
-    }, 500); // 500ms delay
+      // setAllProducts(filteredProducts(allProducts));
+      filteredProducts(allProducts)
+    }, 1000); // 500ms delay
 
     setTypingTimeout(timeout);
-
     return () => {
       if (typingTimeout) {
         clearTimeout(typingTimeout);
       }
     };
-  }, [searchTerm, onSearch]);
+  }, [searchTerm]);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > 0) {
+      // console.log('input change', filteredProducts(allProducts));
+    }
     setSearchTerm(e.target.value);
   };
 
