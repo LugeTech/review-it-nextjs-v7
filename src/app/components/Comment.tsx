d6041c198ec9c63b3cb2f4ef554d717b1f15cf91
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-09-20 13:37:18 -0400

    link to comment user and not review user

--- src/app/components/Comment.tsx
+++ src/app/components/Comment.tsx
@@ -12,12 +12,12 @@ const Comment: React.FC<CommentProps> = ({ comment, user }) => {
     <div className="flex w-full flex-col md:w-3/4 bg-white p-4 rounded-lg shadow-md mb-4">
       <div className="flex items-center mb-2">
         <img
-          src={user.avatar || '/default-avatar.png'} // Use a default avatar if no avatar is provided
+          src={comment?.user?.avatar || '/default-avatar.png'} // Use a default avatar if no avatar is provided
           alt={`${user.firstName} ${user.lastName}`}
           className="w-10 h-10 rounded-full mr-2"
         />
         <span className="text-gray-800 font-semibold">
-          {user.firstName} {user.lastName}
+          {comment?.user?.firstName} {comment?.user?.lastName}
         </span>
       </div>
       <p className="text-gray-700">{comment.body}</p>
