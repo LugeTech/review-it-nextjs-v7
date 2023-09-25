9002d548abcfe48b7836d1e99e47293eca15deef
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-09-25 17:39:27 -0400

    no message

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/signup", "/login", "/api/webhook"],
+  publicRoutes: ["/", "/signup", "/login", "/api/webhook", "/api/webhook/user_update"],
 });
 
 export const config = {
