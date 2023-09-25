4c67ef6451981b880b90883d31f21f7cb8017038
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-09-25 01:41:18 -0400

    not tested throughly but user is added to mongodb on signup/first time signin

--- src/app/util/Interfaces.tsx
+++ src/app/util/Interfaces.tsx
@@ -207,3 +207,46 @@ export interface iCalculatedRating {
   roundedRatingOneDecimalPlace: number;
   numberOfReviews: number;
 }
+
+export interface UserCreatedEvent {
+  object: string;
+  type: string;
+
+  data: {
+    birthday: string;
+    created_at: number;
+    email_addresses: Array<{
+      email_address: string;
+      id: string;
+      linked_to: Array<string>;
+      object: string;
+      verification: {
+        status: string;
+        strategy: string;
+      };
+    }>;
+    external_accounts: Array<string>;
+    external_id: string;
+    first_name: string;
+    gender: string;
+    id: string;
+    image_url: string;
+    last_name: string;
+    last_sign_in_at: number;
+    object: string;
+    password_enabled: boolean;
+    phone_numbers: Array<string>;
+    primary_email_address_id: string;
+    primary_phone_number_id: string | null;
+    primary_web3_wallet_id: string | null;
+    private_metadata: {};
+    profile_image_url: string;
+    public_metadata: {};
+    two_factor_enabled: boolean;
+    unsafe_metadata: {};
+    updated_at: number;
+    username: string | null;
+    web3_wallets: Array<string>;
+  };
+}
+
