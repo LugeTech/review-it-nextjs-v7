b1e482d7522159c023bab6c98fa2c84ee10a187e
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-09-26 11:28:33 -0400

    added user name to user logged in button

--- src/app/components/NavbarAuth.tsx
+++ src/app/components/NavbarAuth.tsx
@@ -2,12 +2,16 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import React, { Suspense } from "react";
 import Skeleton from "react-loading-skeleton";
-
+import { useClerk, useUser } from "@clerk/nextjs";
 const NavbarAuth = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
   return (
-    <>
+    <div className="flex justify-center items-center">
       <Suspense fallback={<Skeleton width={50} height={50} />}>
         <SignedIn>
+          <div className="flex justify-center items-center text-sm mr-1">
+            {user?.username}
+          </div>
           <UserButton />
         </SignedIn>
       </Suspense>
@@ -16,7 +20,7 @@ const NavbarAuth = () => {
           <SignInButton />
         </SignedOut>
       </Suspense>
-    </>
+    </div>
   );
 };
 
