954b925af5591d1f428495b8d8550c22a867a336
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-09-28 12:43:13 -0400

    votes ready to be used with an element

--- src/app/components/Votes.tsx
+++ src/app/components/Votes.tsx
@@ -2,22 +2,25 @@ import { FiThumbsDown, FiThumbsUp } from "react-icons/fi";
 import { iReview } from "../util/Interfaces";
 
 interface VotesProps {
-  review: iReview;
+  element: {
+    helpfulVotes?: number | undefined;
+    unhelpfulVotes?: number | undefined;
+  };
 }
 
-const Votes: React.FC<VotesProps> = ({ review }) => {
+const Votes: React.FC<VotesProps> = ({ element }) => {
   return (
-    <div className=" flex flex-row gap-4">
+    <div className=" flex flex-row gap-4 py-2 justify-start">
       <div className="flex items-center gap-1">
         <FiThumbsUp className="text-myTheme-success" />
         <p className=" text-xs font-light tracking-tight ">
-          {review.helpfulVotes}
+          {element.helpfulVotes}
         </p>
       </div>
       <div className="flex items-center gap-1">
         <FiThumbsDown className="text-myTheme-danger" />
         <p className=" text-xs font-light tracking-tight ">
-          {review.unhelpfulVotes}
+          {element.unhelpfulVotes}
         </p>
       </div>
     </div>
