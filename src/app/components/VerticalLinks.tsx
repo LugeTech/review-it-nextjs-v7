deac212395beb3d4b0c94eb64cf99b5389bcc4ed
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-09-28 13:59:54 -0400

    nil

--- src/app/components/VerticalLinks.tsx
+++ src/app/components/VerticalLinks.tsx
@@ -2,7 +2,7 @@ import { AiOutlineHome, AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
 
 const VerticalLinks = () => {
   return (
-    <div className="flex flex-col justify-between p-2">
+    <div className="flex flex-col justify-between p-2 text-gray-500">
       <AiOutlineHome className="text-3xl" />
       <AiOutlineMail className="text-3xl" />
       <AiOutlinePhone className="text-3xl" />
