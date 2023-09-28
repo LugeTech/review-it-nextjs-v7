b82ffd19c7380aec7d0efcacef6115122a89cdea
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-09-28 17:45:16 -0400

    vertical links look better

--- src/app/components/VerticalLinks.tsx
+++ src/app/components/VerticalLinks.tsx
@@ -2,10 +2,17 @@ import { AiOutlineHome, AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
 
 const VerticalLinks = () => {
   return (
-    <div className="flex flex-col justify-between p-2 text-gray-500">
-      <AiOutlineHome className="text-3xl" />
-      <AiOutlineMail className="text-3xl" />
-      <AiOutlinePhone className="text-3xl" />
+    <div className="flex flex-col justify-between p-2 text-2xl md:text-3xl text-gray-500 border-l-2">
+      <div className="shadow-sm p-2 hover:shadow-md">
+        <AiOutlineHome />
+      </div>
+
+      <div className="shadow-sm p-2 hover:shadow-md">
+        <AiOutlineMail />
+      </div>
+      <div className="shadow-sm p-2 hover:shadow-md">
+        <AiOutlinePhone />
+      </div>
 
     </div>
   )
