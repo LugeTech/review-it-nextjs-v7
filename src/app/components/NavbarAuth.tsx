679c5fd47ceba87b16f98e1aac84715d830b187e
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-09-30 23:44:18 -0400

    removed user name

--- src/app/components/NavbarAuth.tsx
+++ src/app/components/NavbarAuth.tsx
@@ -2,16 +2,11 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import React, { Suspense } from "react";
 import Skeleton from "react-loading-skeleton";
-import { useClerk, useUser } from "@clerk/nextjs";
 const NavbarAuth = () => {
-  const { isLoaded, isSignedIn, user } = useUser();
   return (
     <div className="flex justify-center items-center">
       <Suspense fallback={<Skeleton width={50} height={50} />}>
         <SignedIn>
-          <div className="flex justify-center items-center text-sm mr-1">
-            {user?.username?.toUpperCase()}
-          </div>
           <UserButton />
         </SignedIn>
       </Suspense>
