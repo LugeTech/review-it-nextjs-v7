20ecee57175c68ab3b41d89d6480b7db70a0329a
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-10-20 06:59:07 -0400

    made browse page larger on bigger screens

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -41,7 +41,7 @@ const Page = () => {
 
 
   return (
-    <div className='flex flex-col w-full min-h-screen p-2 md:px-28 sm:pt-8 bg-myTheme-lightbg'>
+    <div className='flex flex-col w-full h-full p-2  sm:pt-8 bg-myTheme-lightbg'>
       <div className='flex flex-col w-full lg:flex-row justify-evenly items-center gap-2'>
 
         <div className='flex flex-col  h-full w-full lg:w-1/4 justify-start items-center gap-2 '>
