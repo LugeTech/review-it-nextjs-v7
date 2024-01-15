2df5a5068c23ccaed9c5938fb80063bd380ba29f
Ken Taylor <kentaylosappdev@gmail.com>
2024-01-15 09:47:00 -0400

    added my likes + bun switch

--- src/app/components/UserInfo.tsx
+++ src/app/components/UserInfo.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { iUser } from '@/app/util/Interfaces';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import { iUser } from "@/app/util/Interfaces";
+import Image from "next/image";
+import Link from "next/link";
 interface UserInfoProps {
   user: iUser;
 }
 
 const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
   const { firstName, lastName, avatar, reviews, comments } = user;
-
 
   return (
     <div className="flex flex-col">
@@ -22,7 +21,9 @@ const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
             height={40}
           />
         )}
-        <h2 className="text-xl font-semibold">{firstName} {lastName}</h2>
+        <h2 className="text-xl font-semibold">
+          {firstName} {lastName}
+        </h2>
       </div>
 
       <div className="mt-4">
@@ -30,7 +31,10 @@ const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
         <ul className="list-disc pl-4">
           {reviews?.map((review) => (
             <li key={review.id}>
-              <Link href={`/fr/${review.id}`} className="text-blue-500 hover:underline">
+              <Link
+                href={`/fr/${review.id}`}
+                className="text-blue-500 hover:underline"
+              >
                 {review.title}
               </Link>
             </li>
@@ -43,8 +47,27 @@ const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
         <ul className="list-disc pl-4">
           {comments?.map((comment) => (
             <li key={comment.id}>
-              <Link href={`/fr/${comment.reviewId}`} className="text-blue-500 hover:underline">
+              <Link
+                href={`/fr/${comment.reviewId}`}
+                className="text-blue-500 hover:underline"
+              >
                 {comment.body}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="mt-4">
+        <h3 className="text-lg font-semibold">My likes</h3>
+        <ul className="list-disc pl-4">
+          {comments?.map((comment) => (
+            <li key={comment.id}>
+              <Link
+                href={`/fr/${comment.reviewId}`}
+                className="text-blue-500 hover:underline"
+              >
+                {comment.body}
+                {comment.helpfulVotes}
               </Link>
             </li>
           ))}
