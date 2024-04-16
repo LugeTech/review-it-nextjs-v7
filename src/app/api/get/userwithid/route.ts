56559fbfde8c8d6fbca5c4221427fae339b6649a
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-04-16 15:50:01 -0400

    properly decode the json

--- src/app/api/get/userwithid/route.ts
+++ src/app/api/get/userwithid/route.ts
@@ -1,20 +1,12 @@
-import { addUserToDb } from "@/app/util/addUserToDb";
 import { prisma } from "@/app/util/prismaClient";
-import { userInDb } from "@/app/util/userInDb";
-import { clerkClient, getAuth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
-interface Body {
-  id: string;
-}
-
 export async function POST(request: NextRequest) {
-  const body = request.body as unknown as Body;
-
+  const userId: string = await request.json();
   try {
     const user = await prisma.user.findUnique({
       where: {
-        id: body.id,
+        id: userId,
       },
       include: {
         comments: true,
