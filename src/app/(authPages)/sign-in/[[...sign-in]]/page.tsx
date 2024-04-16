f6ed45afa7a7754a8f92fe6479adfc31448b3275
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-04-16 16:24:13 -0400

    sign in and signup darkmode

--- src/app/(authPages)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(authPages)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,15 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center mt-8 mb-16 h-full">
-      <SignIn />
+      <SignIn
+        appearance={{
+          elements: {
+            socialButtonsBlockButtonText: "dark:text-white",
+            dividerText: "text-white",
+            footerActionText: "text-white",
+          },
+        }}
+      />
     </div>
   );
 }
--- src/app/(authPages)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(authPages)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,15 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center mt-4">
-      <SignUp />;
+      <SignUp
+        appearance={{
+          elements: {
+            socialButtonsBlockButton: "dark:text-white",
+            dividerText: "text-white",
+            footerActionText: "text-white",
+          },
+        }}
+      />
     </div>
   );
 }
