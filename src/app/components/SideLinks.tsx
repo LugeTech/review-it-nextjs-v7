16a8a9dc0196dbcc7ae882bc69cd0646e63fb2a2
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-05-04 08:41:32 -0400

    add business can now fit

--- src/app/components/SideLinks.tsx
+++ src/app/components/SideLinks.tsx
@@ -9,16 +9,16 @@ import { sideLinks } from "@/app/util/links";
 // }
 //
 const Links = ({ onSideLinkClick }: { onSideLinkClick: () => void }) => {
-
   return (
-    <div className={`flex flex-col mt-20 flex-1 justify-start items-center`}>
-
+    <div className={`flex flex-col mt-20 flex-1 justify-start items-start`}>
       {sideLinks.map((link, index) => (
         <Link href={link.link} key={index}>
-          <li onClick={() => {
-            onSideLinkClick();
-          }}
-            className=" hover:text-myTheme-accent hover:bg-slate-100 duration-400 px-4 transition-all ease-in-out rounded-lg gap-6 py-2">
+          <li
+            onClick={() => {
+              onSideLinkClick();
+            }}
+            className=" hover:text-myTheme-accent hover:bg-slate-100 duration-400 px-4 transition-all ease-in-out rounded-lg gap-6 py-2"
+          >
             {link.name}
           </li>
         </Link>
