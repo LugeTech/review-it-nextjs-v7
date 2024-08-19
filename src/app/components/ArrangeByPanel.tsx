60dd380145f6b03c2e7a3a0e838546bf3c9633cf
Clint Eastman <clint@Clints-MacBook-Air.local>
2024-08-19 11:43:39 -0400

    started to design filter search page

--- src/app/components/ArrangeByPanel.tsx
+++ src/app/components/ArrangeByPanel.tsx
@@ -1,18 +1,28 @@
-
 const ArrangeByPanel = () => {
   return (
-    <div className='flex flex-1 flex-col p-2 w-full bg-myTheme-lightbg dark:bg-myTheme-niceBlack dark:text-myTheme-light'>
-      <div className='flex w-full'>
-        <p className='flex-1 text-center'>Arrange by rating</p>
+    <div className="flex flex-1 flex-col p-2 w-full bg-myTheme-lightbg dark:bg-myTheme-niceBlack dark:text-myTheme-light">
+      <div className="flex w-full">
+        <p className="flex-1 text-center">Arrange by rating</p>
       </div>
       <div className="btn-group flex w-full ">
-        <button className="btn flex-1 bg-myTheme-secondary text-myTheme-dark ">Any</button>
-        <button className="btn flex-1 bg-myTheme-primary text-myTheme-dark ">3.0+</button>
-        <button className="btn flex-1 bg-myTheme-primary text-myTheme-dark ">4.0+</button>
-        <button className="btn flex-1 bg-myTheme-primary text-myTheme-dark ">4.5+</button>
+        <button className="btn flex-1 bg-myTheme-secondary text-myTheme-dark ">
+          Any
+        </button>
+        <button className="btn flex-1 bg-myTheme-primary text-myTheme-dark ">
+          3.0+
+        </button>
+        <button className="btn flex-1 bg-myTheme-primary text-myTheme-dark ">
+          4.0+
+        </button>
+        <button className="btn flex-1 bg-myTheme-primary text-myTheme-dark ">
+          4.5+
+        </button>
+      </div>
+      <div className="flex w-full">
+        <input type="checkbox" value="carrental" /> Car Rental
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ArrangeByPanel
+export default ArrangeByPanel;
