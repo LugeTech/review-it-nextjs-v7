b258be9202fd419e778410ab7a32bf9492e216b4
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-08-30 18:05:06 -0400

    make view review button taller

--- src/app/components/ReviewBox.tsx
+++ src/app/components/ReviewBox.tsx
@@ -28,14 +28,14 @@ const ReviewBox: React.FC<ReviewBoxProps> = ({ review: review }) => {
   return (
     <div className="bg-white border border-gray-200 rounded-lg p-3 sm:p-4 shadow-sm hover:shadow-md transition-shadow duration-200 w-full">
       <div className="flex flex-col sm:flex-row items-start sm:space-x-4">
-        <div className="flex items-center w-full sm:w-auto mb-2 sm:mb-0">
+        <div className="flex items-center w-full sm:w-auto mb-2 sm:mb-0 gap-2">
           {review?.user && (
             <Image
               src={review.user.avatar!}
               alt="User avatar"
-              width={40}
-              height={40}
-              className="rounded-full object-cover w-10 h-10 sm:w-12 sm:h-12 mr-3 sm:mr-0"
+              width={60}
+              height={60}
+              className="rounded-full object-cover w-16 h-16 sm:w-12 sm:h-12 mr-3 sm:mr-0"
             />
           )}
           <div className="flex-grow sm:hidden">
@@ -86,7 +86,7 @@ const ReviewBox: React.FC<ReviewBoxProps> = ({ review: review }) => {
             </p>
             <Link
               href={`/fr?id=${review?.id}&productid=${review?.product?.id}`}
-              className="bg-blue-600 text-white hover:bg-blue-700 px-3 py-1 rounded transition-colors duration-200 w-full sm:w-auto text-center"
+              className="bg-blue-600 text-white hover:bg-blue-700 px-3 py-2 rounded transition-colors duration-200 w-full sm:w-auto text-center"
             >
               View review
             </Link>
