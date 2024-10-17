b4f4897cb27e8fa712fc2a1d29577c29d1da2b91
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-10-17 15:42:39 -0400

    refactor: improve code readability in BusinessDashboardComponent

--- src/components/business-dashboard.tsx
+++ src/components/business-dashboard.tsx
@@ -1,17 +1,33 @@
-"use client"
-import { StarIcon, TrendingUpIcon, TrendingDownIcon, BellIcon, RocketIcon } from "lucide-react"
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Badge } from "@/components/ui/badge"
-import { useQuery } from "@tanstack/react-query"
-import { useAuth } from "@clerk/nextjs"
-import { getUser, getNotifications } from "@/app/util/serverFunctions"
-import LoadingSpinner from "@/app/components/LoadingSpinner"
-import { iProductOwnerNotification, iUser, iUserNotification } from "@/app/util/Interfaces"
-import Link from "next/link"
-import { FaPlus } from 'react-icons/fa';
-import NotificationBell from "@/app/components/notification-components/OwnerNotification"
-import { ownerNotificationsAtom } from "@/app/store/store"
-import { useAtomValue } from 'jotai'
+"use client";
+import {
+  StarIcon,
+  TrendingUpIcon,
+  TrendingDownIcon,
+  BellIcon,
+  RocketIcon,
+} from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "@clerk/nextjs";
+import { getUser, getNotifications } from "@/app/util/serverFunctions";
+import LoadingSpinner from "@/app/components/LoadingSpinner";
+import {
+  iProductOwnerNotification,
+  iUser,
+  iUserNotification,
+} from "@/app/util/Interfaces";
+import Link from "next/link";
+import { FaPlus } from "react-icons/fa";
+import NotificationBell from "@/app/components/notification-components/OwnerNotification";
+import { ownerNotificationsAtom } from "@/app/store/store";
+import { useAtomValue } from "jotai";
 
 export function BusinessDashboardComponent() {
   const auth = useAuth();
@@ -28,8 +44,8 @@ export function BusinessDashboardComponent() {
   const user: iUser | undefined = data?.data as iUser;
 
   // get all a users products from all their businesses
-  const products = user.businesses?.flatMap(business => business.products) || [];
-
+  const products =
+    user.businesses?.flatMap((business) => business.products) || [];
 
   // This might come in handy later, this keeps tha business umbrella
   //   const businessProducts = user.businesses?.map(business => ({
@@ -42,12 +58,15 @@ export function BusinessDashboardComponent() {
       <div className="container mx-auto p-6 bg-gray-100 min-h-screen flex items-center justify-center">
         <Card className="w-full max-w-2xl ">
           <CardHeader>
-            <CardTitle className="text-3xl font-bold text-center">Contribute to Review It</CardTitle>
+            <CardTitle className="text-3xl font-bold text-center">
+              Contribute to Review It
+            </CardTitle>
           </CardHeader>
           <CardContent className="text-start">
             <RocketIcon className="w-24 h-24 mx-auto text-blue-500 mb-6" />
             <p className="text-base md:text-xl mb-4">
-              Help local businesses thrive by adding them to our platform. Anyone can contribute!
+              Help local businesses thrive by adding them to our platform.
+              Anyone can contribute!
             </p>
             <ul className="text-base md:text-xl text-start mb-4 list-disc pl-6 space-y-2">
               <li>Support small businesses in your community</li>
@@ -56,25 +75,34 @@ export function BusinessDashboardComponent() {
               <li>Empower businesses with valuable customer feedback</li>
               <li>Foster local economic growth through increased visibility</li>
             </ul>
-            <Link href="/submit" className="bg-blue-600 text-white hover:bg-blue-700 py-3 px-6 rounded-full text-lg font-semibold transition-colors duration-300 shadow-lg hover:shadow-xl">
+            <Link
+              href="/submit"
+              className="bg-blue-600 text-white hover:bg-blue-700 py-3 px-6 rounded-full text-lg font-semibold transition-colors duration-300 shadow-lg hover:shadow-xl"
+            >
               Add a Business
             </Link>
           </CardContent>
           <CardFooter className="text-center text-gray-600">
-            Join our community effort to boost local businesses and help them reach their full potential!
+            Join our community effort to boost local businesses and help them
+            reach their full potential!
           </CardFooter>
         </Card>
       </div>
-    )
+    );
   }
 
   return (
     <div className="container mx-auto p-6 bg-gray-100 min-h-screen">
-      <h1 className=" text-3xl font-bold mb-6 text-center">Your Business Dashboard</h1>
+      <h1 className=" text-3xl font-bold mb-6 text-center">
+        Your Business Dashboard
+      </h1>
 
       {/* Add Business Button */}
       <div className="flex justify-center mb-6">
-        <Link href="/submit" className="flex items-center bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors">
+        <Link
+          href="/submit"
+          className="flex items-center bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors"
+        >
           <FaPlus className="w-4 h-4 mr-2" />
           Add New Business
         </Link>
@@ -85,15 +113,21 @@ export function BusinessDashboardComponent() {
           <Card key={product.id} className="flex flex-col">
             <CardHeader>
               <CardTitle className="flex justify-between items-center">
-                <span className="text-xl font-bold truncate">{product.name}</span>
-                <Badge className="text-white bg-black" variant="secondary">{product.tags.slice(0, 1).join(", ") || "No Category"}</Badge>
+                <span className="text-xl font-bold truncate">
+                  {product.name}
+                </span>
+                <Badge className="text-white bg-black" variant="secondary">
+                  {product.tags.slice(0, 1).join(", ") || "No Category"}
+                </Badge>
               </CardTitle>
             </CardHeader>
             <CardContent className="flex-grow">
               <div className="flex items-center mb-2">
                 <StarIcon className="w-5 h-5 text-yellow-400 mr-1" />
                 <span className="font-bold">{product.rating.toFixed(1)}</span>
-                <span className="text-gray-600 text-sm ml-2">({product.reviews?.length || 0} reviews)</span>
+                <span className="text-gray-600 text-sm ml-2">
+                  ({product.reviews?.length || 0} reviews)
+                </span>
                 {product.rating >= 3 ? (
                   <TrendingUpIcon className="w-4 h-4 text-green-500 ml-2" />
                 ) : (
@@ -101,13 +135,32 @@ export function BusinessDashboardComponent() {
                 )}
               </div>
               <div className="flex items-center mb-2 text-sm text-gray-600">
-                <NotificationBell notifications={notifications.filter((notification: iProductOwnerNotification) => notification.product_id === product.id) || []} />
-                <span className="ml-2 text-sm text-gray-600">new notifications</span>
+                <NotificationBell
+                  notifications={
+                    notifications.filter(
+                      (notification: iProductOwnerNotification) =>
+                        notification.product_id === product.id &&
+                        notification.read === false,
+                    ) || []
+                  }
+                />
+                <span className="ml-2 text-sm text-gray-600">
+                  new notifications
+                </span>
               </div>
               <p className="text-sm text-gray-700 mb-2" />
               <p className="text-sm text-gray-700 mb-2">
-                <span className="font-semibold">Latest review:</span> &quot;{product.reviews?.[0]?.title || "No reviews yet"}&quot;
-                <span className="text-gray-600 text-sm ml-2">({product.reviews?.[0]?.createdDate ? new Date(product.reviews?.[0]?.createdDate).toLocaleDateString() : null})</span>
+                <span className="font-semibold">Latest review:</span> &quot;
+                {product.reviews?.[0]?.title || "No reviews yet"}&quot;
+                <span className="text-gray-600 text-sm ml-2">
+                  (
+                  {product.reviews?.[0]?.createdDate
+                    ? new Date(
+                        product.reviews?.[0]?.createdDate,
+                      ).toLocaleDateString()
+                    : null}
+                  )
+                </span>
               </p>
               {/* <p className="text-gray-600 text-sm">{product.description.length > 100 ? `${product.description.substring(0, 100)}...` : product.description}</p> */}
             </CardContent>
@@ -120,5 +173,5 @@ export function BusinessDashboardComponent() {
         ))}
       </div>
     </div>
-  )
+  );
 }
